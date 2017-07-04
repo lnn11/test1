c6226d016e4a0119a64691732d1fa8d4d41aa7f5
lnn11 <lnn15001092587@163.com>
2017-07-04 23:29:04 +0800

    ssss

--- test/src/com/test/controller/test.java
+++ test/src/com/test/controller/test.java
@@ -3,8 +3,7 @@ package com.test.controller;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("12456sss");
-		System.out.println("qezfcdbdfbfv");
+		System.out.println("12456lllsss");
 		System.out.println("qezfcdbdfbfv");
 	}
 }
